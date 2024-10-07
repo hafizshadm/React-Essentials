8622d8ea7d256bd1724b49d8f888190490a876ad
Shad Muhammad <hafizshad.pk@gmail.com>
2024-10-07 11:19:02 +0500

    Core Concepts component is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
-import reactImage from './assets/react-core-concepts.png';
+import reactImage from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+// While adding props there must be passing 1 value, we destructure single object here
+function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-        <img src={reactImage} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
 }
-
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
